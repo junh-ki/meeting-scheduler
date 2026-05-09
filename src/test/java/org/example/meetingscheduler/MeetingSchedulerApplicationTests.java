e986ca9ccf8b1ba42c17eaeba892da5f940d8019
Junhyung Ki <kijoonh91@gmail.com>
2026-05-09 10:48:55 +0200

    Init the project

---
+++ src/test/java/org/example/meetingscheduler/MeetingSchedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.meetingscheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeetingSchedulerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
